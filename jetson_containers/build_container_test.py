f345186c0644519cdf6b7db87b20ec345f6fa5ab
olimissia <oli@missia.care>
2025-03-25 16:51:43 +0100

    refactor: enhance container build scripts with improved logging and validation

--- jetson_containers/build_container_test.py
+++ jetson_containers/build_container_test.py
@@ -1,25 +1,127 @@
 #!/usr/bin/env python3
-import os
-import sys
-import copy
-import time
-import json
-import pprint
-import fnmatch
-import logging
-import traceback
-import subprocess
+# import os
+# import sys
+# import copy
+# import time
+# import json
+# import pprint
+# import fnmatch
+# import logging
+# import traceback
+# import subprocess
 # import dockerhub_api 
 
-from .packages import find_package, find_packages, resolve_dependencies, validate_dict, _PACKAGE_ROOT
-from .l4t_version import L4T_VERSION, l4t_version_from_tag, l4t_version_compatible, get_l4t_base
-from .utils import split_container_name, query_yes_no, needs_sudo, sudo_prefix
-from .logging import log_dir, log_debug, pprint_debug
+# from .packages import validate_dict 
+#,  find_package, find_packages, resolve_dependencies,  #, _PACKAGE_ROOT
+# from .l4t_version import L4T_VERSION, l4t_version_from_tag, l4t_version_compatible, get_l4t_base
+# from .utils import split_container_name, query_yes_no, needs_sudo, sudo_prefix
+# from .logging import log_dir, log_debug, pprint_debug
 
 # from packaging.version import Version
 
+def validate_dict(package):
+    """
+    Return true if this is a package configuration dict.
+    """
+    if not isinstance(package, dict):
+        return False
+        
+    for key, value in package.items():
+        if key not in _PACKAGE_KEYS:
+            #print(f"-- Unknown key '{key}' in package config:", value)
+            return False
+            
+    return True
 
-def build_container(name, packages) : #, base=get_l4t_base(), build_flags='', build_args=None, simulate=False, skip_tests=[], test_only=[], push='', no_github_api=False):
+import os
+import re
+import sys
+import json
+import platform
+import subprocess
+import glob
+
+from packaging.version import Version
+
+
+def get_l4t_version(version_file='/etc/nv_tegra_release'):
+    """
+    Returns the L4T_VERSION in a packaging.version.Version object
+    Which can be compared against other version objects:  https://packaging.pypa.io/en/latest/version.html
+    You can also access the version components directly.  For example, on L4T R35.3.1:
+    
+        version.major == 35
+        version.minor == 3
+        version.micro == 1
+        
+    The L4T_VERSION will either be parsed from /etc/nv_tegra_release or the $L4T_VERSION environment variable.
+    """
+    if platform.machine() != 'aarch64':
+        raise ValueError(f"L4T_VERSION isn't supported on {ARCH} architecture (aarch64 only)")
+        
+    if 'L4T_VERSION' in os.environ and len(os.environ['L4T_VERSION']) > 0:
+        return Version(os.environ['L4T_VERSION'].lower().lstrip('r'))
+        
+    if not os.path.isfile(version_file):
+        raise IOError(f"L4T_VERSION file doesn't exist:  {version_file}")
+        
+    with open(version_file) as file:
+        line = file.readline()
+        
+    # R32 (release), REVISION: 7.1, GCID: 29689809, BOARD: t186ref, EABI: aarch64, DATE: Wed Feb  2 21:33:23 UTC 2022
+    # R34 (release), REVISION: 1.1, GCID: 30414990, BOARD: t186ref, EABI: aarch64, DATE: Tue May 17 04:20:55 UTC 2022
+    # R35 (release), REVISION: 2.1, GCID: 32398013, BOARD: t186ref, EABI: aarch64, DATE: Sun Jan 22 03:18:23 UTC 2023
+    # R35 (release), REVISION: 3.1, GCID: 32790763, BOARD: t186ref, EABI: aarch64, DATE: Wed Mar 15 07:54:12 UTC 2023
+    parts = [part.strip() for part in line.split(',')]
+
+    # parse the release
+    l4t_release = parts[0]
+    l4t_release_prefix = '# R'
+    l4t_release_suffix = ' (release)'
+    
+    if not l4t_release.startswith(l4t_release_prefix) or not l4t_release.endswith(l4t_release_suffix):
+        raise ValueError(f"L4T release string is invalid or in unexpected format:  '{l4t_release}'")
+        
+    l4t_release = l4t_release[len(l4t_release_prefix):-len(l4t_release_suffix)]
+
+    # parse the revision
+    l4t_revision = parts[1]
+    l4t_revision_prefix = 'REVISION: '
+    
+    if not l4t_revision.startswith(l4t_revision_prefix):
+        raise ValueError(f"L4T revision '{l4t_revision}' doesn't start with expected prefix '{l4t_revision_prefix}'")
+       
+    l4t_revision = l4t_revision[len(l4t_revision_prefix):]
+    
+    # return packaging.version object
+    return Version(f'{l4t_release}.{l4t_revision}')
+    
+
+
+
+def get_l4t_base(l4t_version=get_l4t_version()):
+    """
+    Returns the l4t-base or l4t-jetpack container to use
+    """
+    print("l4t_version is ", l4t_version)
+    if l4t_version.major >= 36:   # JetPack 6
+        return "ubuntu:22.04" #"nvcr.io/ea-linux4tegra/l4t-jetpack:r36.0.0"
+    elif l4t_version.major >= 34: # JetPack 5
+        if l4t_version >= Version('35.4.1'):
+            return "nvcr.io/nvidia/l4t-jetpack:r35.4.1"
+        else:
+            return f"nvcr.io/nvidia/l4t-jetpack:r{l4t_version}"
+    else:
+        if l4t_version >= Version('32.7.1'):
+            return "nvcr.io/nvidia/l4t-base:r32.7.1"
+        else:
+            return f"nvcr.io/nvidia/l4t-base:r{l4t_version}"
+            
+
+
+
+
+def build_container(name, packages, base=get_l4t_base()) : #, build_flags='', build_args=None, simulate=False, skip_tests=[], test_only=[], push='', no_github_api=False):
     """
     Multi-stage container build that chains together selected packages into one container image.
     For example, `['pytorch', 'tensorflow']` would build a container that had both pytorch and tensorflow in it.
@@ -45,13 +147,18 @@ def build_container(name, packages) : #, base=get_l4t_base(), build_flags='', bu
     elif validate_dict(packages):
         packages = [packages['name']]
 
-    print(f"Building container {name} with packages {packages} and base {base}")
-        
-# build_container(name, packages) 
+    print(f"Building container {name} with packages {packages} with base {base}")
+
+name = 'test_container'
+packages = ['pytorch', 'transformers']
+
+
+build_container(name, packages) 
 
 
 
-    """
+
+"""
     if len(packages) == 0:
         raise ValueError("must specify at least one package to build")    
         
@@ -175,4 +282,4 @@ def build_container(name, packages) : #, base=get_l4t_base(), build_flags='', bu
      
     print(f"-- Done building container {name}")        
     return name"
-    """
+"""
